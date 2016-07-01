fcac7f9315d490e7111df4f81de78ae4b5868b9b
github username <github email address>
2016-07-01 10:34:24 -0400

    advanced part completed as well

--- locationTrivia-dataStore/FISAddLocationViewController.m
+++ locationTrivia-dataStore/FISAddLocationViewController.m
@@ -7,12 +7,16 @@
 //
 
 #import "FISAddLocationViewController.h"
+#import <CoreLocation/CoreLocation.h>
 
-@interface FISAddLocationViewController ()
+@interface FISAddLocationViewController () <CLLocationManagerDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *nameField;
 @property (weak, nonatomic) IBOutlet UITextField *latitudeField;
 @property (weak, nonatomic) IBOutlet UITextField *longitudeField;
 
+@property (nonatomic) CLLocationManager *locationManager;
+@property (nonatomic) CLGeocoder *geocoder;
+@property (nonatomic) int locationFetchCounter;
 
 @end
 
@@ -20,6 +24,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    self.geocoder = [[CLGeocoder alloc] init];
+    //we need to ask for authorization
+    [self.locationManager requestWhenInUseAuthorization];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -41,5 +51,38 @@
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
+
+// execute this method to start fetching location - connected to the "add my location" button
+- (IBAction)doFetchCurrentLocation:(id)sender {
+    self.locationFetchCounter = 0;
+    
+    // fetching current location start from here
+    [self.locationManager startUpdatingLocation];
+    
+}
+
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+    // this delegate method is constantly invoked every some miliseconds.
+    // we only need to receive the first response, so we skip the others.
+    if (self.locationFetchCounter > 0) return;
+    self.locationFetchCounter++;
+    
+    // after we have current coordinates, we use this method to fetch the information data of fetched coordinate
+    [self.geocoder reverseGeocodeLocation:[locations lastObject] completionHandler:^(NSArray *placemarks, NSError *error) {
+        CLPlacemark *placemark = [placemarks lastObject];
+        
+        self.nameField.text = placemark.name;
+        self.latitudeField.text = [NSString stringWithFormat:@"%f", placemark.location.coordinate.latitude];
+        self.longitudeField.text = [NSString stringWithFormat:@"%f", placemark.location.coordinate.longitude];
+        
+        // stopping locationManager from fetching again
+        [self.locationManager stopUpdatingLocation];
+    }];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    NSLog(@"failed to fetch current location : %@", error);
+}
 
 @end
